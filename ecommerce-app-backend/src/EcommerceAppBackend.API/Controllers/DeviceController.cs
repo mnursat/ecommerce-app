54e3e7ac965e2676a0d137279236643d1a6f8390
Nursat <arbuzenjoyeer@gmail.com>
2025-10-09 15:39:49 +0500

    feat: add backend and base model (device)

---
+++ ecommerce-app-backend/src/EcommerceAppBackend.API/Controllers/DeviceController.cs
@@ -0,0 +1,18 @@
+﻿namespace EcommerceAppBackend.API.Controllers;
+
+using EcommerceAppBackend.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DeviceController : ControllerBase
+{
+    [HttpGet]
+    public async Task<List<Device>> GetDevicesAsync()
+    {
+        var devices = new List<Device>();
+
+
+        return devices;
+    }
+}
---
+++ ecommerce-app-backend/src/EcommerceAppBackend.API/Models/Device.cs
@@ -0,0 +1,12 @@
+﻿namespace EcommerceAppBackend.API.Models;
+
+public class Device
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
