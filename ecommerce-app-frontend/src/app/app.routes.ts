11e3faf26a998d5088e3d6835145da47efc7d70c
Nursat <arbuzenjoyeer@gmail.com>
2025-10-09 11:31:58 +0500

    feat: add dockerfile and device page

--- ecommerce-app-frontend/src/app/app.routes.ts
+++ ecommerce-app-frontend/src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: Home
-    },
-    {
-        path: 'home',
-        redirectTo: '',
-        pathMatch: 'full'
-    }
+   {
+    path: '',
+    loadComponent: () =>
+        import('./pages/home/home').then((m) => m.Home),
+   },
+   {
+      path: 'devices',
+      loadComponent: () =>
+         import('./pages/devices/devices').then((m) => m.Devices),
+   },
 ];
